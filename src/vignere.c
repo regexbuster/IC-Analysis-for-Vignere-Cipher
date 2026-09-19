6373caa9264d06f8a17fd55324558dc6d45d72dc
regexbuster <benjamin@gofranks.com>
2026-09-19 01:22:38 -0400

    vignere first finish

--- src/vignere.c
+++ src/vignere.c
@@ -1,5 +1,19 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void strToUpper(char* str){
+    for (int i = 0; i < strlen(str); i++){
+        str[i] = toupper(str[i]);
+    }
+}
+
+void strToLower(char* str){
+    for (int i = 0; i < strlen(str); i++){
+        str[i] = tolower(str[i]);
+    }
+}
 
 char ShiftEnc(char key, char msgLetter){
     int absKey, absLetter, shifted;
@@ -25,20 +39,16 @@ char ShiftDec(char key, char ctLetter){
     return (char)('a' + shifted);
 }
 
-/*
-0 - successful
-1 - buffer too short
-*/
 int VignereEncrypt(char* key, char* message, char* msgBuffer){
     int keyPos, keyLength, msgLength;
 
     keyLength = strlen(key);
     msgLength = strlen(message);
 
-    if (msgLength > strlen(msgBuffer)){
-        printf("Unable to encrypt as output buffer is too small.\n");
-        return 1;
-    }
+    // if (msgLength > strlen(msgBuffer)){
+    //     printf("Unable to encrypt as output buffer is too small.\n");
+    //     return 1;
+    // }
 
     for(int i = 0; i < msgLength; i++){
         keyPos = i % keyLength;
@@ -49,13 +59,53 @@ int VignereEncrypt(char* key, char* message, char* msgBuffer){
     return 0;
 }
 
-void VignereDecrypt(char* key, char* ciphertext, char* ctBuffer){
+int VignereDecrypt(char* key, char* ciphertext, char* ctBuffer){
+    int keyPos, keyLength, msgLength;
 
+    keyLength = strlen(key);
+    msgLength = strlen(ciphertext);
+
+    // if (msgLength > strlen(msgBuffer)){
+    //     printf("Unable to encrypt as output buffer is too small.\n");
+    //     return 1;
+    // }
+
+    for(int i = 0; i < msgLength; i++){
+        keyPos = i % keyLength;
+
+        ctBuffer[i] = ShiftDec(key[keyPos], ciphertext[i]);
+    }
+
+    return 0;
 }
 
 int main(int argc, char* argv[]){
     if (argc < 4){
         printf("Usage: vignere [enc/dec] [KEY] [MESSAGE]\n");
+        exit(1);
+    }
+
+    if (!strcmp("enc", argv[1])){
+        strToLower(argv[2]);
+        strToLower(argv[3]);
+        char* buffer = malloc(strlen(argv[3]) * sizeof(char));
+
+        VignereEncrypt(argv[2], argv[3], buffer);
+
+        printf("ENC: %s\n", buffer);
+        free(buffer);
+    } else if (!strcmp("dec", argv[1])){
+        strToUpper(argv[2]);
+        strToUpper(argv[3]);
+        char* buffer = malloc(strlen(argv[3]) * sizeof(char));
+
+        VignereDecrypt(argv[2], argv[3], buffer);
+
+        printf("DEC: %s\n", buffer);
+        free(buffer);
+    } else {
+        printf("Usage: vignere [enc/dec] [KEY] [MESSAGE]\n");
+        printf("Unknown option: %s\n", argv[1]);
         exit(1);
     }
 }
--- src/vignere.h
+++ src/vignere.h
@@ -1,1 +1,13 @@
 #pragma once
+
+void strToUpper(char* str);
+
+void strToLower(char* str);
+
+char ShiftEnc(char key, char msgLetter);
+
+char ShiftDec(char key, char ctLetter);
+
+int VignereEncrypt(char* key, char* message, char* msgBuffer);
+
+int VignereDecrypt(char* key, char* ciphertext, char* ctBuffer);
